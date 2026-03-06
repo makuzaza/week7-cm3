62dd8917a1c43cd24402605b0cada8c1b9b3f3cc
Markus Kannisto <m.kannisto01@gmail.com>
2026-03-06 11:44:31 +0200

    feature(tests): Implement GET and POST tests

--- backend_V1/index.js
+++ backend_V1/index.js
@@ -1,10 +1,6 @@
 const app = require('./app');
-const connectDB = require('./config/db');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-
-// Connect to MongoDB
-connectDB();
 
 // Start server
 const server = app.listen(config.PORT, () => {
