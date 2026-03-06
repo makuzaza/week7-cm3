9894ae2699a020d14c3458bc2b0113d62f89f422
Minoo Yaghoubi <minoo.yaghoubi@gmail.com>
2026-03-06 12:22:49 +0200

    Implement fetching vehicle rentals from API

--- frontend_V1/src/components/VehicleRentalListings.jsx
+++ frontend_V1/src/components/VehicleRentalListings.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
 import VehicleRentalListing from "./VehicleRentalListing";
 
 const VehicleRentalListings = () => {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const fechProperties = async () => {
+      const prpproperties = await fetch("/api/vehicleRentals");
+      const data = await prpproperties.json();
+      setProperties(data);
+    };
+    fechProperties();
+  }, []);
+
   return (
     <div className="rental-list">
-      <VehicleRentalListing />
+      {properties.map((property) => (
+        <VehicleRentalListing key={property.id} property={property} />
+      ))}
     </div>
   );
 };
