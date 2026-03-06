ae5e70038fbdc13205857cf4d69ad8437de82712
Minoo Yaghoubi <minoo.yaghoubi@gmail.com>
2026-03-06 12:18:08 +0200

    Add Navbar component

--- frontend_V1/src/components/Navbar.jsx
+++ frontend_V1/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Vehicle Rental</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/add-rental">Add Rental</a>
+        <Link to="/add-rental">Add Rental</Link>
       </div>
     </nav>
   );
