04f4e39c096994ca0948458da32e973edc0bcdca
Minoo Yaghoubi <minoo.yaghoubi@gmail.com>
2026-03-06 14:17:32 +0200

    Update routes for rental pages

--- frontend_V1/src/App.jsx
+++ frontend_V1/src/App.jsx
@@ -1,26 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// pages & components
-import Home from "./pages/HomePage";
-import AddVehicleRentalPage from "./pages/AddVehicleRentalPage";
 import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import AddVehicleRentalPage from "./pages/AddVehicleRentalPage";
+import EditVehicleRentalPage from "./pages/EditVehicleRentalPage";
+import VehicleRentalPage from "./pages/VehicleRentalPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-const App = () => {
+function App() {
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/add-rental" element={<AddVehicleRentalPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
+
+    <Router>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/add-rental" element={<AddVehicleRentalPage />} />
+
+        <Route path="/edit-rental/:id" element={<EditVehicleRentalPage />} />
+
+        <Route path="/vehicle-rental/:id" element={<VehicleRentalPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+
+      </Routes>
+
+    </Router>
+
   );
-};
+
+}
 
 export default App;
