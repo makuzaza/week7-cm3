c8970a4d45a0df50e9d88f6b58e15a69feace7a2
Minoo Yaghoubi <minoo.yaghoubi@gmail.com>
2026-03-06 14:16:37 +0200

    Improve vehicle rental listing and add edit/delete buttons

--- frontend_V1/src/components/VehicleRentalListings.jsx
+++ frontend_V1/src/components/VehicleRentalListings.jsx
@@ -5,21 +5,35 @@ const VehicleRentalListings = () => {
   const [properties, setProperties] = useState([]);
 
   useEffect(() => {
-    const fechProperties = async () => {
-      const prpproperties = await fetch("/api/vehicleRentals");
-      const data = await prpproperties.json();
+    const fetchProperties = async () => {
+      const response = await fetch("/api/vehicleRentals");
+      const data = await response.json();
       setProperties(data);
     };
-    fechProperties();
+
+    fetchProperties();
   }, []);
+
+  const handleDelete = async (id) => {
+    await fetch(`/api/vehicleRentals/${id}`, {
+      method: "DELETE",
+    });
+
+    setProperties(properties.filter((p) => p._id !== id));
+  };
 
   return (
     <div className="rental-list">
       {properties.map((property) => (
-        <VehicleRentalListing key={property.id} property={property} />
+        <VehicleRentalListing
+          key={property._id}
+          property={property}
+          onDelete={handleDelete}
+        />
       ))}
     </div>
   );
 };
 
 export default VehicleRentalListings;
+
